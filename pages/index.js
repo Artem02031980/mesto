0ed5e8a86ff710258a6760e7e501b1c3cb61f0f0
Artem Khmelev <artem0203@yandex.ru>
2022-12-07 22:00:54 +0300

    bugfix: Исправлены ошибки - Ревью 1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-const openPopupEdit = document.querySelector('.button_type_edit')
-const closePopup = document.querySelector('.button_form_close')
-const overlay = document.querySelector('.overlay')
+const openPopupEdit = document.querySelector('.author__button-edit')
+const closePopup = document.querySelector('.popup__button-close')
 const popup = document.querySelector('.popup')
 
 const authorName = document.querySelector('.author__name')
@@ -9,13 +8,12 @@ const authorBiz = document.querySelector('.author__biz')
 const nameInput = document.querySelector('#add-one')
 const jobInput = document.querySelector('#add-two')
 
-const formElement = document.querySelector('.form_edit-profile')
+const formElement = document.querySelector('.form')
 
 function togglePopup () {
-  overlay.classList.toggle('overlay_opened')
   popup.classList.toggle('popup_opened')
 
-  if(overlay.classList.contains('overlay_opened') && popup.classList.contains('popup_opened')){
+  if(popup.classList.contains('popup_opened')){
     nameInput.value = authorName.textContent
     jobInput.value = authorBiz.textContent
   }
@@ -27,8 +25,7 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   authorName.textContent = nameInput.value
   authorBiz.textContent = jobInput.value
-  overlay.classList.remove('overlay_opened')
-  popup.classList.remove('popup_opened')
+  togglePopup()
 }
 formElement.addEventListener('submit', formSubmitHandler);
 
